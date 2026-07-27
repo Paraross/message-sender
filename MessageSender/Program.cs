9e451c453da15f0ae600ffdde2c823649eeab97d
Adrian Bednorz <a.bednorz@wasko.pl>
2026-07-27 11:11:34 +0200

    Test automatic device detection

--- MessageSender/Program.cs
+++ MessageSender/Program.cs
@@ -2,53 +2,124 @@
 using System.Net.Sockets;
 using System.Text;
 
+const int defaultPort = 6969;
+var port = int.Parse(args.ElementAtOrDefault(1) ?? defaultPort.ToString());
+
+var localIpAddress = GetLocalIpAddress();
+
+if (localIpAddress == null)
+{
+    Console.WriteLine("Couldn't get local IP address, aborting");
+    return;
+}
+
+Console.WriteLine($"Current IP address: {localIpAddress}");
+Console.WriteLine($"Endpoint: {new IPEndPoint(localIpAddress, port)}");
+
+if (args.Length == 0)
+{
+    Console.WriteLine("'wait' or 'detect'");
+    return;
+}
+
 var mode = args[0];
 
-const int port = 6969;
-
-if (mode == "send")
+if (mode == "wait")
 {
-    var address = args[1];
-    var ipAddress = IPAddress.Parse(address);
-    var ipEndPoint = new IPEndPoint(ipAddress, port);
-    
-    using TcpClient client = new();
-    
-    await client.ConnectAsync(ipEndPoint);
-    await using var stream = client.GetStream();
-    
-    var message = args[2];
-    var bytes = Encoding.UTF8.GetBytes(message);
-    
-    await stream.WriteAsync(bytes);
-    
-    Console.WriteLine($"Sent {bytes.Length} bytes");
-    Console.WriteLine($"Message sent: \"{message}\"");   
+    await WaitAndRespond();
 }
-else if (mode == "listen")
+else if (mode == "send")
 {
-    var ipEndPoint = new IPEndPoint(IPAddress.Any, port);
-    TcpListener listener = new(ipEndPoint);
+    SendOne();
+}
+else if (mode == "detect")
+{
+    await DetectAvailableDevices();
+}
+else
+{
+    Console.WriteLine("Incorrect command");
+}
 
-    try
-    {    
-        Console.WriteLine("Listening...");
-        listener.Start();
+return;
 
-        using var handler = await listener.AcceptTcpClientAsync();
-        await using var stream = handler.GetStream();
+IPAddress? GetLocalIpAddress()
+{
+    using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+    socket.Connect("8.8.8.8", 65530);
+    var endPoint = socket.LocalEndPoint as IPEndPoint;
+    var localIpAddress = endPoint?.Address;
 
-        var buffer = new byte[1024];
+    return localIpAddress;
+}
 
-        var read = await stream.ReadAsync(buffer);
+void SendOne()
+{
+    var udpClient = new UdpClient();
+    udpClient.Client.Bind(new IPEndPoint(localIpAddress, port));
 
-        var message = Encoding.UTF8.GetString(buffer);
-        
-        Console.WriteLine($"Got {read} bytes");
-        Console.WriteLine($"Message: \"{message}\"");
-    }
-    finally
+    const string message = "ident";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+
+    var broadcastAddress = new IPEndPoint(IPAddress.Broadcast, port);
+    udpClient.Send(messageBytes, messageBytes.Length, broadcastAddress);
+}
+
+Task DetectAvailableDevices()
+{
+    var udpClient = new UdpClient();
+    udpClient.Client.Bind(new IPEndPoint(localIpAddress, port));
+
+    const string message = "ident";
+    var messageBytes = Encoding.UTF8.GetBytes(message);
+
+    var receiveResponseTask = Task.Run(() =>
     {
-        listener.Stop();
-    }  
+        while (true)
+        {
+            // send broadcast
+            var broadcastAddress = new IPEndPoint(IPAddress.Broadcast, port);
+            udpClient.Send(messageBytes, messageBytes.Length, broadcastAddress);
+
+            var from = new IPEndPoint(0, 0);
+
+            // receive response
+            var receivedBytes = udpClient.Receive(ref from);
+            var receivedString = Encoding.UTF8.GetString(receivedBytes);
+
+            if (!from.Equals(new IPEndPoint(localIpAddress, port)))
+            {
+                Console.WriteLine($"received: '{receivedString}' from {from}");
+            }
+
+            Thread.Sleep(500);
+        }
+    });
+
+    return receiveResponseTask;
+}
+
+Task WaitAndRespond()
+{
+    var udpClient = new UdpClient();
+    udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, port));
+
+    var waitAndRespondTask = Task.Run(() =>
+    {
+        while (true)
+        {
+            var from = new IPEndPoint(0, 0);
+            var receivedBytes = udpClient.Receive(ref from);
+            var receivedString = Encoding.UTF8.GetString(receivedBytes);
+
+            Console.WriteLine(
+                receivedString == "ident" ? $"received ident from: {from}" : $"received other message from: {from}");
+
+            const string response = "IP";
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+            udpClient.Send(responseBytes, responseBytes.Length, from);
+        }
+    });
+
+    return waitAndRespondTask;
 }
